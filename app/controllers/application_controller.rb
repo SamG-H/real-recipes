bf9fe7ec7d7ddc8bc0a94d10056cc375699ad1ba
Sam Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-08-06 11:24:07 -0700

    add login/signup pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,12 +28,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/account" do
-    @user = User.find(session[:user_id])
-    erb :account
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      erb :account
+    else
+      redirect "/login"
+    end
   end
 
   get "/login" do
-    erb: login
+    erb :login
   end
 
   post "/login" do
@@ -44,6 +48,11 @@ class ApplicationController < Sinatra::Base
     else
       erb :login
     end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
   end
 
 end
