9b4ceac3723ee81bb102826eab95e8f812495b26
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-08-10 13:40:19 -0700

    update edit route to deny access to other non-owner users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,15 +21,13 @@ class ApplicationController < Sinatra::Base
     if (params[:username] == "" || params[:password] == "")
       @error = "You must type in a username and a password"
       erb :signup
+    elsif User.find_by(username: params[:username])
+      @error = "Username taken"
+      erb :signup
     else
-      if User.find_by(username: params[:username])
-        @error = "Username taken"
-        erb :signup
-      else
-        user = User.create(username: params[:username], password: params[:password])
-        session[:user_id] = user.id
-        redirect "/recipes"
-      end
+      user = User.create(username: params[:username], password: params[:password])
+      session[:user_id] = user.id
+      redirect "/recipes"
     end
   end
 
@@ -77,11 +75,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/:id/edit' do
-    if logged_in?
-      @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find(params[:id])
+    if @recipe.user_id == current_user.id
       erb :edit
     else
-      redirect "/login"
+      redirect "/uhoh"
     end
   end
 
@@ -92,6 +90,7 @@ class ApplicationController < Sinatra::Base
         erb :uhoh
       elsif @recipe.user_id == current_user.id
         @recipe = Recipe.find(params[:id])
+        User.find(@recipe.user_id)
         erb :show
       else
         erb :uhoh
@@ -103,15 +102,23 @@ class ApplicationController < Sinatra::Base
 
   patch '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
-    @recipe.update(name: params[:name], cook_time: params[:cook_time], ingredients: params[:ingredients], 
-    tags: params[:tags], link: params[:link], color: params[:color])
-    redirect "/recipes/#{@recipe.id}"
+    if @recipe.user_id == current_user.id
+      @recipe.update(name: params[:name], cook_time: params[:cook_time], ingredients: params[:ingredients], 
+                     tags: params[:tags], link: params[:link], color: params[:color])
+      redirect "/recipes/#{@recipe.id}"
+    else
+      redirect "/uhoh"
+    end
   end
 
   delete '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
-    @recipe.destroy
-    redirect to '/recipes'
+    if @recipe.user_id == current_user.id
+      @recipe = Recipe.find(params[:id])
+      @recipe.destroy
+      redirect '/recipes'
+    else
+      redirect "/uhoh"
+    end
   end
 
   helpers do
