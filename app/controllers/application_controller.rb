23fc8cf8ea7279c005d14693059b056c9c8859a3
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-08-16 12:57:54 -0700

    add route for page not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,30 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "password_security"
+    set :session_secret, "SESSION_KEY"
   end
 
   get "/" do
       erb :index
   end
-  
+
+  not_found do
+    status 404
+    erb :uhoh
+  end
+
   helpers do
     def logged_in?
       !!session[:user_id]
     end
 
     def current_user
-      User.find(session[:user_id])
+      User.find_by(id: session[:user_id])
     end
   end
 
