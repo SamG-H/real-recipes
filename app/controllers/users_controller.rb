c57c9fdb128c4a7e10c94c60dedb8426a8ca7a5f
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-01-23 12:45:42 -0800

    fix user signup bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   post "/signup" do
     @user = User.new(params)
-    if user.save
+    if @user.save
       session[:user_id] = @user.id
       redirect to "/recipes"
     else
